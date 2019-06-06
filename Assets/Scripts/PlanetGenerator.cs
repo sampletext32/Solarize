d9b1736a76c6269591f0f964f250683f31b04754
sampletext32 <sampletext32@bk.ru>
2019-06-06 03:39:32 +0300

    Planet Generator Version 1

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -30,23 +30,35 @@ public class PlanetGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < CountPlanets; i++)
+        int maxPlanets = 10;
+
+        int countPlanets = Random.Range(0, maxPlanets + 1);
+
+        HashSet<int> planetsIndices = new HashSet<int>();
+
+        while (planetsIndices.Count < countPlanets)
         {
-            float RiRatio = SpaceMaths.GetPlanetRadiusByTitius_Bode(i);
+            planetsIndices.Add(Random.Range(0, maxPlanets + 1));
+        }
+
+        foreach (var planetsIndex in planetsIndices)
+        {
+            float RiRatio = SpaceMaths.GetPlanetRadiusByTitius_Bode(planetsIndex);
             float R = Constants.UnityUnitConverter.FromAstronomicalUnits(RiRatio);
             float T = SpaceMaths.YearsToSeconds(SpaceMaths.KeplerLawPeriod(RiRatio, 1f));
             //Debug.Log(T);
             GameObject planet = Instantiate(SpherePrefab);
-            planet.name = "Planet " + (i + 1);
+            planet.name = "Planet " + (planetsIndex + 1);
             OrbitControl control = planet.GetComponent<OrbitControl>();
             CreateOrbitPoints(planet.GetComponent<LineRenderer>(), R);
             float phi_0 = Random.Range(0, Mathf.PI * 2);
             PlanetData data = new PlanetData(R, T, phi_0);
             control.SetPlanetData(data);
         }
+        
 
-        //Camera.main.GetComponent<CameraSetupper>().target = GameObject.Find("Center").transform;
-        Camera.main.GetComponent<CameraSetupper>().target = GameObject.Find("Planet 3").transform;
+        Camera.main.GetComponent<CameraSetupper>().target = GameObject.Find("Center").transform;
+        //Camera.main.GetComponent<CameraSetupper>().target = GameObject.Find("Planet 3").transform;
     }
 
     // Update is called once per frame
